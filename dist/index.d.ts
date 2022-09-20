d1c0367f681dad029158040f885a06e501dec2b0
Terry Fei <i@terryfei.com>
2022-09-21 03:08:44 +0800

    rm dist

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="hyper-function-component" />
-export declare function vueToHfc(Comp: any, opts: {
-    tag: string;
-    props: [string[], string[], string[]];
-    connected?: (container: Element, props: HfcProps) => void;
-    disconnected?: () => void;
-}): typeof HyperFunctionComponent;
